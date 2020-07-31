b924b081b1f334d48f063d0363afc0096d50087a
Andreas Triantafyllos <andreas.triantafyllos@iohk.io>
2020-07-31 16:51:17 +0300

    cleanup 2

--- build_vocab.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Note that this script uses GNU-style sed. On Mac OS, you are required to first
-#    brew install gnu-sed --with-default-names
-
-# Creates a file "vocab.txt" containing tokens presented in data and how many times a token appears
-
-cat data/train_pos.txt data/train_neg.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > vocab.txt
